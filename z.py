9ea688fc09f32afc5e62557f14714330bb1626fc
WeakMouse <felixhuang07@gmail.com>
2023-06-10 18:31:12 +0800

    Done

--- z.py
+++ z.py
@@ -17,30 +17,25 @@ def DictToArray(Dict):
 def PieChart(title, labels, data):
 	global idx
 
-	if len(data) > 10:
+	if len(labels) > 10:
 		return
 
-	plt.figure(idx)
-	idx += 1
 	fig, ax = plt.subplots(figsize = (6, 6))
 	ax.set_title(title, fontsize = 18)
 
 	ax.pie(data,
 		   labels = labels,
 		   autopct = '%.1f%%',
-		   wedgeprops = {'linewidth': 3.0, 'edgecolor': 'white'},
-		   textprops = {'size': 'x-large'})
+		   wedgeprops = {'linewidth': 3.0, 'edgecolor': 'white'})
 
 def BarChart(title, labels, data):
 	global idx
 
-	if len(data) > 10:
+	if len(labels) > 10:
 		return
 
 	x = [i + 1 for i in range(len(labels))]
 
-	plt.figure(idx)
-	idx += 1
 	fig, ax = plt.subplots(figsize = (6, 6))
 	ax.set_title(title, fontsize = 18)
 
@@ -49,48 +44,82 @@ def BarChart(title, labels, data):
 		   tick_label = labels,
 		   width = 0.5)
 
-def LineGraph(title, x, y):
+def LineGraph(x_name, x, y_name, y):
+	global idx
+
+	data = [[x[i], y[i]] for i in range(len(x))]
+	data.sort()
+	for i in range(len(x)):
+		x[i] = data[i][0]
+		y[i] = data[i][1]
+		if i > 0 and x[i] == x[i - 1]:
+			return
+
+	plt.figure(idx)
+	idx += 1
+
+	print(x, y)
+
+	plt.xlim(x[0], x[-1])
+	plt.ylim(min(y), max(y))
+	plt.plot(x, y)
+	plt.grid()
+	plt.xlabel(x_name, fontsize = 12)
+	plt.ylabel(y_name, fontsize = 12)
+
+def ScatterGraph(x_name, x, y_name, y):
 	global idx
 
 	plt.figure(idx)
 	idx += 1
-	fig, ax = plt.subplots(figsize = (6, 6))
-	ax.set_title(title, fontsize = 18)
 
-	ax.plot(x, y)
-
-def SparseGraph(title, x, y):
-	global idx
-
-	plt.figure(idx)
-	idx += 1
-	fig, ax = plt.subplots(figsize = (6, 6))
-	ax.set_title(title, fontsize = 18)
+	plt.scatter(x, y, alpha = 0.5)
+	plt.grid()
+	plt.xlabel(x_name, fontsize = 12)
+	plt.ylabel(y_name, fontsize = 12)
 
 with open('data.csv', newline = '') as csvfile:
 	rows = list(csv.reader(csvfile))
 
 	labels = []
+	types = []
 	for l in rows[0]:
 		labels.append(l)
+		types.append(1)
 
 	Dict = {}
 	for i in range(len(labels)):
 		Dict[labels[i]] = {}
 
+	cols = [[] for _ in range(len(labels))]
 	for i in range(1, len(rows)):
 		for j in range(len(labels)):
 			if not(rows[i][j] in Dict[labels[j]]):
 				Dict[labels[j]][rows[i][j]] = 0
 			Dict[labels[j]][rows[i][j]] += 1
+			cols[j].append(rows[i][j])
+			try:
+				int(rows[i][j])
+			except:
+				types[j] = 0
 
-	# print(Dict['birth_count'])
-	# PieChart('sex', Dict['sex'])
-	for k in Dict.keys():
-		labels, data = DictToArray(Dict[k])
-		PieChart(k, labels, data)
-		BarChart(k, labels, data)
-		LineGraph(k, labels, data)
+	for i in range(len(types)):
+		if types[i] == 1:
+			for j in range(len(cols[i])):
+				cols[i][j] = int(cols[i][j])
 
-plt.tight_layout()
+	for i in range(len(labels)):
+		k = labels[i]
+		L, D = DictToArray(Dict[k])
+		if types[i] <= 1:
+			PieChart(k, L, D)
+			BarChart(k, L, D)
+		
+	for i in range(len(labels)):
+		if types[i] == 1:
+			for j in range(i + 1, len(labels)):
+				if types[j] == 1:
+					LineGraph(labels[i], cols[i], labels[j], cols[j])
+					ScatterGraph(labels[i], cols[i], labels[j], cols[j])
+
 plt.show()
