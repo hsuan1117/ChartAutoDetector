fb39c22636217169274494a348d8683379454e0f
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-06-10 17:06:56 +0800

    UI Done

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 from werkzeug.utils import secure_filename
 from wtforms import SubmitField
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -24,6 +25,11 @@ class DataForm(FlaskForm):
     submit = SubmitField("送出")
 
 
+def gen_id():
+    import random, string
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+
 @app.route('/')
 def home_landing():
     return render_template('home.html')
@@ -36,11 +42,42 @@ def upload_image():
         f = form.file.data
         print(f)
         filename = secure_filename(f.filename)
-        f.save(os.path.join(
-            app.instance_path, 'files', filename
-        ))
-        return redirect(url_for('success'))
+        temp_dir = TemporaryDirectory().name
+        if not os.path.exists(os.path.join(temp_dir, 'files')):
+            os.makedirs(os.path.join(temp_dir, 'files'))
+        path = os.path.join(
+            temp_dir, 'files', filename
+        )
+        f.save(path)
+        data = {
+            "id": gen_id(),
+            "about": form.about.data,
+            "path": path,
+            "time": datetime.now()
+        }
+        if 'upload' in session:
+            session['upload'].append(data)
+        else:
+            session['upload'] = [data]
+        return redirect(url_for('my'))
     return render_template('use.html', form=form)
+
+
+@app.get('/my')
+def my():
+    if 'upload' in session:
+        return render_template('my.html', files=session['upload'])
+    else:
+        return render_template('my.html', files=[])
+
+
+@app.get('/show/<id>')
+def show(id):
+    if 'upload' in session:
+        for data in session['upload']:
+            if data['id'] == id:
+                return render_template('show.html', data=data)
+    return redirect(url_for('my'))
 
 
 if __name__ == '__main__':
